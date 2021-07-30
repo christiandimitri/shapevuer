4622ee44f730695265f1e3f4ed18453d6c0f15ce
Christian <christian.j.dimitrii@gmail.com>
2021-07-30 22:22:25 +0200

    Added cli to dev dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Banner>Fundebug: The most professional application error monitoring platform!</Banner>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Banner from "@/components/Banner";
 
 export default {
   name: 'App',
   components: {
+    Banner,
     HelloWorld
   }
 }
